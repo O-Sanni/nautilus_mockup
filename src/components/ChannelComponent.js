79beeab9354e433f5435a2c4ba24614576fcf1f0
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-28 14:57:16 -0400

    Second Row Channels

--- src/components/ChannelComponent.js
+++ src/components/ChannelComponent.js
@@ -17,21 +17,60 @@ render()
                 <p>by Adam Piare</p>
             </div>
             <div>
-            <h3>Women in Science & Engineering</h3>
-            <img src ={require("../assets/wise_sci_and_engineering.png")} alt="woman" />
-            <p>Your Barain Is On the Brink of Chaos</p>
-            <p>by Kelly Clancy</p>
+                <h3>Women in Science & Engineering</h3>
+                <img src ={require("../assets/wise_sci_and_engineering.png")} alt="woman" />
+                <p>Your Barain Is On the Brink of Chaos</p>
+                <p>by Kelly Clancy</p>
             </div>
             <div>
-            <h3>QUANTA ABSTRACTIONS</h3>
-            <img src ={require("../assets/abstractions.jpeg")} alt="woman" />
-            <p>Mathematicians Calculate How Randomness Creeps In</p>
-            <p>by Marcus Woo</p>
+                <h3>QUANTA ABSTRACTIONS</h3>
+                <img src ={require("../assets/abstractions.jpeg")} alt="woman" />
+                <p>Mathematicians Calculate How Randomness Creeps In</p>
+                <p>by Marcus Woo</p>
             </div>
         </div>
-
-
+        <div>
+            <div>
+                <h3>EARTH</h3>
+                <img src={require("../assets/bio_and_beyond.jpeg")} alt="biology" />
+                <p>The Deep Time of Walden Pond</p>
+                <p>by Curt Stager</p>
+            </div>
+            <div>
+                <h3>THINK LIKE A SCIENTIST</h3>
+                <img src ={require("../assets/wise_sci_and_engineering.png")} alt="woman" />
+                <img src ={require("../assets/Audio_sprite.png")} alt="play" />
+                <p>The Best Burger Place Is a Lab</p>
+                <p>by Thomas King</p>
+            </div>
+            <div>
+                <h3>cosmos</h3>
+                <img src ={require("../assets/abstractions.jpeg")} alt="woman" />
+                <p>How Much Should Expectation Drive Science?</p>
+                <p>by Claudia Geib</p>
+            </div>
         </div>
+        <div>
+        <div>
+                <h3>POETRY IN SCIENCE</h3>
+                <img src={require("../assets/bio_and_beyond.jpeg")} alt="biology" />
+                <p>Making Sense of the Genome, at Last</p>
+                <p>by Adam Piare</p>
+            </div>
+            <div>
+                <h3>Women in Science & Engineering</h3>
+                <img src ={require("../assets/wise_sci_and_engineering.png")} alt="woman" />
+                <p>Your Barain Is On the Brink of Chaos</p>
+                <p>by Kelly Clancy</p>
+            </div>
+            <div>
+                <h3>QUANTA ABSTRACTIONS</h3>
+                <img src ={require("../assets/abstractions.jpeg")} alt="woman" />
+                <p>Mathematicians Calculate How Randomness Creeps In</p>
+                <p>by Marcus Woo</p>
+            </div>
+        </div>
+    </div>
 
     )
 }

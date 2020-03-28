1c67e225dff7f331451351333f57a6c82c0e31fd
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-28 13:59:02 -0400

    Add Channel Component

---
+++ src/components/ChannelComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+class ChannelComponents extends  React.Component{
+render()
+{
+    return(
+        <div>
+        <h1>Channels</h1>
+        <h2>TUNE IN TO NAUTILUS' DEEP DIVE PORTALS</h2>
+        <div>
+            <div>
+                <h1>BIOLOGY+BEYOND</h1>
+                <img src="/src/assets/bio_and_beyond.jpeg" alt="biologyImg" />
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+
+        </div>
+
+    )
+}
+}
--- src/components/TopOfPage.js
+++ src/components/TopOfPage.js
@@ -5,7 +5,7 @@ class TopOfPage extends React.Component{
     render(){
         return(
             <div id="topPage" >
-            <img id="topPageImg" src= "/src/assets/matches.png"  alt= "Matches" />
+            
             </div>
         )
     }

1e4181b4681f7edd197cffa783f0daad9ec78288
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-28 13:39:34 -0400

    wireframe

---
+++ src/components/TopOfPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./components.css";
+
+class TopOfPage extends React.Component{
+    render(){
+        return(
+            <div id="topPage" >
+            <img id="topPageImg" src= "/src/assets/matches.png"  alt= "Matches" />
+            </div>
+        )
+    }
+}
+
+export default TopOfPage;
